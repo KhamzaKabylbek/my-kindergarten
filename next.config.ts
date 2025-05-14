ae32721acdc8a8deb5205f133737cceca5aa6065
KhamzaKabylbek <xamza110011@gmail.com>
2025-05-14 14:53:54 +0500

    Добавлен workflow для CI и деплоя на GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
+  basePath: '/my-kindergarten',  // Путь для деплоя на GitHub Pages
 };
 
 module.exports = nextConfig;
